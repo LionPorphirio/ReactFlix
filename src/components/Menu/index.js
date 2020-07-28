8ea5b61be51f76625ba85afb6f4404f89a80f877
Lion Porphirio <lion.porphirio@gmail.com>
2020-07-27 22:28:36 -0300

    troca do logo, conteúdo, adição de componentes

--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import Logo from '../../assets/img/Logo.png';
+import Lionflix from '../../assets/img/Lionflix.png';
 import './Menu.css'
 import Button from './components';
 
@@ -7,7 +7,7 @@ function Menu() {
     return (
         <nav className="Menu">
             <a href="/">
-                <img className="Logo" src={Logo} alt="AluraFlix logo"/>
+                <img className="Logo" src={Lionflix} alt="AluraFlix logo"/>
             </a>
 
             <Button as="a" className="ButtonLink" href="/">
